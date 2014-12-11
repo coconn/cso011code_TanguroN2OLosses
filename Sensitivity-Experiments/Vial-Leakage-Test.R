0f1d6fa26e15784c20951442e84f92f44e344002
Christine O'Connell <coconn@umn.edu>
2014-12-11 12:37:51 -0600

    Leakage test code
    created a bar graph draft version

--- Sensitivity-Experiments/Vial-Leakage-Test.R
+++ Sensitivity-Experiments/Vial-Leakage-Test.R
@@ -77,9 +77,132 @@ write.csv(leakagetestsummary, file=paste(pathsavefiles, "leakagetestsummary.csv"
 
 
 ########################################################################
-# Bar graphs looking at each time point (Leak0 - Leak12)
+# Bar graph looking at each time point (Leak0 - Leak12)
+
+# summarySE
+# Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%)
+# http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/#helper-functions
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  require(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+dt <- leakagetestvials
+dtc <- summarySE(leakagetestvials, measurevar="N2Oppm", groupvars=c("GCrun","SampleName"), na.rm=TRUE)
+
+# Use GCrun as a factor rather than numeric
+dtc2 <- dtc
+dtc2$GCrun <- factor(dtc2$GCrun)
+
+# Error bars represent standard error of the mean
+ggplot(dtc2, aes(x=GCrun, y=N2Oppm, fill=SampleName)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=N2Oppm-se, ymax=N2Oppm+se),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
 
 
+
+
+
+
+
+
+
+
+# summarySE
+# Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%)
+# http://www.cookbook-r.com/Graphs/Plotting_means_and_error_bars_(ggplot2)/#helper-functions
+summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
+                      conf.interval=.95, .drop=TRUE) {
+  require(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+df <- ToothGrowth
+dfc <- summarySE(df, measurevar="len", groupvars=c("supp","dose"))
+
+library(ggplot2)
+
+# Use dose as a factor rather than numeric
+dfc2 <- dfc
+dfc2$dose <- factor(dfc2$dose)
+
+# Error bars represent standard error of the mean
+ggplot(dfc2, aes(x=dose, y=len, fill=supp)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=len-se, ymax=len+se),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+
+# Use 95% confidence intervals instead of SEM
+ggplot(dfc2, aes(x=dose, y=len, fill=supp)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=len-ci, ymax=len+ci),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
 
 
 
