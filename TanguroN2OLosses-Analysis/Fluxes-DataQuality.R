265de145dd6b2b8badfccc8f1b72853f23bd9e77
Christine O'Connell <coconn@umn.edu>
2015-02-25 09:47:11 -0600

    Christine made some changes (methane)

--- TanguroN2OLosses-Analysis/Fluxes-DataQuality.R
+++ TanguroN2OLosses-Analysis/Fluxes-DataQuality.R
@@ -22,8 +22,10 @@ QuadR2_N2O <- fluxesfull$QuadR2[(fluxesfull$GasType)=="N2O"]
 LinearR2_CO2 <- fluxesfull$LinearR2[(fluxesfull$GasType)=="CO2"]
 QuadR2_CO2 <- fluxesfull$QuadR2[(fluxesfull$GasType)=="CO2"]
 
+LinearR2_CH4 <- fluxesfull$LinearR2[(fluxesfull$GasType)=="CH4"]
+QuadR2_CH4 <- fluxesfull$QuadR2[(fluxesfull$GasType)=="CH4"]
 
-## histograms of CO2 and N2O R^2 values
+## histograms of CO2, CH4 and N2O R^2 values
 
 # where to save figure
 pathsavefigures = "~/Documents/GITHUB/cso011code_TanguroN2OLosses/TanguroN2OLosses-Analysis/Fluxes-DataQuality/"
@@ -32,11 +34,13 @@ pathsavefigures = "~/Documents/GITHUB/cso011code_TanguroN2OLosses/TanguroN2OLoss
 png(file = paste(pathsavefigures, "R2-fluxes-histograms.png", sep=""),width=6,height=6,units="in",res=400)
 
 # make plot
-par(mfrow=c(2,2),oma=c(0,0,2,0),mar = c(5.1, 4.1, 2.1, 2.1))
+par(mfrow=c(3,2),oma=c(0,0,2,0),mar = c(5.1, 4.1, 2.1, 2.1))
 hist(LinearR2_N2O, main="")
 hist(QuadR2_N2O, main="")
 hist(LinearR2_CO2, main="")
 hist(QuadR2_CO2, main="")
+hist(LinearR2_CH4, main="")
+hist(QuadR2_CH4, main="")
 title(substitute(paste(R^2, " Histograms, Sample Size = ", samplenum, " chambers"), list(samplenum = samplenum)), outer=TRUE)
 
 dev.off()
@@ -44,7 +48,7 @@ dev.off()
 
 
 ########################################################################
-# DATA QUALITY: CO2 and N2O CORRELATION
+# DATA QUALITY: CO2, N2O AND CH4 CORRELATION
 
 ## correlation between the CO2 and N2O R^2 values
 # correlation info
@@ -87,6 +91,27 @@ legend('topleft', legend = paste("ρ =", round(cor1t$estimate,4), cor_stars[1]),
 plot(QuadR2_CO2,QuadR2_N2O)
 abline(r2quadlm)
 legend('topleft', legend = paste("ρ =", round(cor2t$estimate,4), cor_stars[2]), bty = 'n')
+
+# # plot 3-4 is CO2 vs CH4
+# #plot3
+# plot(LinearR2_CO2,LinearR2_CH4)
+# abline(r2linearlm)
+# legend('topleft', legend = paste("ρ =", round(cor1t$estimate,4), cor_stars[1]), bty = 'n')
+# #plot4
+# plot(QuadR2_CO2,QuadR2_N2O)
+# abline(r2quadlm)
+# legend('topleft', legend = paste("ρ =", round(cor2t$estimate,4), cor_stars[2]), bty = 'n')
+# 
+# # plot 5-6 is N2O vs CH4
+# #plot5
+# plot(LinearR2_CO2,LinearR2_N2O)
+# abline(r2linearlm)
+# legend('topleft', legend = paste("ρ =", round(cor1t$estimate,4), cor_stars[1]), bty = 'n')
+# #plot6
+# plot(QuadR2_CO2,QuadR2_N2O)
+# abline(r2quadlm)
+# legend('topleft', legend = paste("ρ =", round(cor2t$estimate,4), cor_stars[2]), bty = 'n')
+
 
 dev.off()
 
